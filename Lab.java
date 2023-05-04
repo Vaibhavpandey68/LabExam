4eb9f9b3428a9b06177bc59f13d1bbc494565b09
Vaibhavpandey68 <vaibhavpandey689@gmail.com>
2023-05-04 21:47:34 +0530

    Lab.java

---
+++ Lab.java
@@ -0,0 +1,13 @@
+ class Lab {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y=20;
+                int z=30;
+		int b = x * y * z;
+		System.out.println("Value =" +b);
+
+
+	}
+
+}
